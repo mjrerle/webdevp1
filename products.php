35023b5cc97abb33eb948504af0f913878ee62fa
mjrerle <matterle@live.com>
2017-02-17 14:27:31 -0700

    attempt to fix last commit

--- products.php
+++ products.php
@@ -1,13 +1,6 @@
 <?php
 include 'templates/header.php';
-<<<<<<< HEAD
 $ingredientstore = simplexml_load_file("ingredients.xml") or die("Error: Cannot create object.");
-=======
-include 'includes/comments.php';
-
-$ingredientstore = simplexml_load_file("ingredients.xml") or die("Error: Cannot create object.");
-
->>>>>>> f2307949895493cd6e123333399d7e94adcd0e42
 if (isset($_GET['id'])) {
     # show product specifics
     $id = $_GET['id'];
@@ -18,9 +11,5 @@ if (isset($_GET['id'])) {
     # show product listing
     include 'templates/products_listing.php';
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> f2307949895493cd6e123333399d7e94adcd0e42
 include 'templates/footer.php';
 ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,15 +1,7 @@
 <footer class="footer">
-<<<<<<< HEAD
       <div class="container">
         <span class="text-muted">Place sticky footer content here.</span>
       </div>
     </footer>
-=======
-    <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
-    </div>
-</footer>
-
->>>>>>> f2307949895493cd6e123333399d7e94adcd0e42
 </body>
 </html>
